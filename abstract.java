338122d5c0c3f51283b5c42f1ee4e3f302617209
Shanmukhi-katta <168467375+Shanmukhi-katta@users.noreply.github.com>
2024-05-02 21:40:39 +0530

    abstract.java

--- abstract.java
+++ abstract.java
@@ -1,5 +1,30 @@
 package com.coreconcepts.abstractclass;
 
+public class Veg extends Person{
+	
+	@Override
+	public void eat() {
+		System.out.println("Eats veg");
+	}
+
+}
+public class Nonveg extends Person {
+	
+
+	@Override
+	public void eat() {
+		System.out.println("Eats Nonveg");
+	}
+
+}
+public class Veg extends Person{
+	
+	@Override
+	public void eat() {
+		System.out.println("Eats veg");
+	}
+
+}
 public class MainAbstract {
 	
 	public static void main(String[] args) {

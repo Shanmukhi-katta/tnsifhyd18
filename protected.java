23810e1b109be2349e3eba7cb0e04a9e05dd3260
Shanmukhi-katta <168467375+Shanmukhi-katta@users.noreply.github.com>
2024-05-02 20:46:41 +0530

    protected.java

--- protected.java
+++ protected.java
@@ -7,3 +7,13 @@ public class ProtectedA {
 	}
 
 }
+public class Protected2Main extends ProtectedA {
+	
+	public static void main(String[] args) {
+		
+		Protected2Main p3 = new Protected2Main();
+		p3.display();
+		
+	}
+
+}

4215753f9a5225e60bb1a5e4643cf0c574e6a9e5
Shanmukhi-katta <168467375+Shanmukhi-katta@users.noreply.github.com>
2024-05-16 12:57:21 +0530

    CRUD-DeleteEmp.java

--- CRUD-DeleteEmp.java
+++ CRUD-DeleteEmp.java
@@ -5,39 +5,38 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class UpdateEmp {
+public class DeleteEmp {
 	public static void main(String[] args) {
 		String dbURL = "jdbc:mysql://localhost:3306/sample";
 		String username = "root";
 		String password = "root";
+		 
 		try {
 		 
 		    Connection conn = DriverManager.getConnection(dbURL, username, password);
-		    String sql = "UPDATE emptable SET emp_name=?, emp_age=? WHERE emp_id=? ";
-		    String sql1 = "UPDATE emptable SET emp_name=?, emp_age=? WHERE emp_id=? ";
-		    
+		    String sql = "DELETE FROM emptable WHERE emp_id=?";
+		    String sql1 = "DELETE FROM emptable WHERE emp_id=?";
 		    
 		    
 		    PreparedStatement statement = conn.prepareStatement(sql);
-		    statement.setString(1, "Pooja");
-		    statement.setString(2, "19");
-		    statement.setString(3, "101");
+		    PreparedStatement statement1 = conn.prepareStatement(sql1);
 		    
-		    PreparedStatement statement1 = conn.prepareStatement(sql);
-		    statement1.setString(1, "Pavi");
-		    statement1.setString(2, "21");
-		    statement1.setString(3, "102");
-		    
+		    statement.setString(1, "103");
+		    statement1.setString(1, "104");
 		     
-		    int rowsUpdated = statement.executeUpdate();
-		    int rowsUpdated1= statement1.executeUpdate();
 		    
-		    if (rowsUpdated > 0) {
-		        System.out.println("An existing first employee was updated successfully!");
+		    int rowsDeleted = statement.executeUpdate();
+		    int rowsDeleted1 = statement1.executeUpdate();
+		    
+		    if (rowsDeleted > 0) {
+		        System.out.println("first employee was deleted successfully!");
 		    }
-		    if (rowsUpdated > 0) {
-		        System.out.println("An existing second employee was updated successfully!");
+		    
+		    if (rowsDeleted1> 0) {
+		        System.out.println("second employee was deleted successfully!");
 		    }
+		    
+		    
 		} catch (SQLException ex) {
 		    ex.printStackTrace();
 		}
